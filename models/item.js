20807da9d8ee32c91536be9aa5a384ba3f0beae6
Liridon Loku <liridonloku@gmail.com>
2022-05-08 21:47:20 +0200

    Create Item model

---
+++ models/item.js
@@ -0,0 +1,18 @@
+let mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number, required: true },
+  number_in_stock: { type: Number, required: true },
+});
+
+// Virtual for item url
+ItemSchema.virtual("url").get(function () {
+  return "/catalog/item/" + this._id;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
