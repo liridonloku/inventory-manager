41569833f0967c734371c3528dffdaff7a062893
Liridon Loku <liridonloku@gmail.com>
2022-05-18 18:20:27 +0200

    Add update item page

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -104,6 +104,7 @@ exports.item_create_post = [
     .trim()
     .isLength({ min: 1 })
     .escape(),
+
   // Handle request
   function (req, res, next) {
     const errors = validationResult(req);
@@ -115,6 +116,7 @@ exports.item_create_post = [
       price: req.body.price,
       number_in_stock: req.body.number_in_stock,
     });
+
     // Error
     if (!errors.isEmpty()) {
       // Re-render form
@@ -140,9 +142,86 @@ exports.item_create_post = [
           });
         }
       );
-    } else {
+    }
+    // Success
+    else {
+      // Save document and redirect
+      item.save(function (err) {
+        if (err) return next(err);
+        res.redirect(item.url);
+      });
+    }
+  },
+];
+
+// Item update form on GET
+exports.item_update_get = function (req, res, next) {
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.params.id).exec(callback);
+      },
+      manufacturers: function (callback) {
+        Manufacturer.find({}).sort({ name: 1 }).exec(callback);
+      },
+      categories: function (callback) {
+        Category.find({}).sort({ name: 1 }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      res.render("item_form", {
+        title: `Update Item: ${results.item.name}`,
+        item: results.item,
+        manufacturers: results.manufacturers,
+        categories: results.categories,
+      });
+    }
+  );
+};
+
+// Item update form POST
+exports.item_update_post = [
+  // Validate and sanitize
+  body("name", "Name cannot be empty").trim().isLength({ min: 1 }).escape(),
+  body("manufacturer", "Manufacturer cannot be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Category cannot be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Description cannot be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price cannot be empty").trim().isLength({ min: 1 }).escape(),
+  body("number_in_stock", "Number in stock cannot be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Handle request
+  function (req, res, next) {
+    const errors = validationResult(req);
+    const item = new Item({
+      name: req.body.name,
+      manufacturer: req.body.manufacturer,
+      category: req.body.category,
+      description: req.body.description,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      _id: req.params.id,
+    });
+
+    // Error
+    if (!errors.isEmpty()) {
       async.parallel(
         {
+          oldItem: function (callback) {
+            Item.findById(req.params.id).exec(callback);
+          },
           manufacturers: function (callback) {
             Manufacturer.find({}).sort({ name: 1 }).exec(callback);
           },
@@ -153,27 +232,26 @@ exports.item_create_post = [
         function (error, results) {
           if (error) return next(error);
 
-          // Save document and redirect
-          item.save(function (err) {
-            if (err) return next(err);
-            res.redirect(item.url);
+          res.render("item_form", {
+            title: `Update item: ${results.oldItem.name}`,
+            item: item,
+            categories: results.categories,
+            manufacturers: results.manufacturers,
+            errors: errors.array(),
           });
         }
       );
     }
+    // Success
+    else {
+      // Update item and redirect
+      Item.findByIdAndUpdate(req.params.id, item, {}, function (err, item) {
+        if (err) return next(err);
+        res.redirect(item.url);
+      });
+    }
   },
-  // Success
 ];
-
-// Item update form on GET
-exports.item_update_get = function (req, res, next) {
-  res.send("GET Update item: Not implemented.");
-};
-
-// Item update form POST
-exports.item_update_post = function (req, res, next) {
-  res.send("POST Update item: Not implemented.");
-};
 
 // Item delete form on GET
 exports.item_delete_get = function (req, res, next) {
